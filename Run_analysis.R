5f6625f4494d12075dac7fff8e41bb539a4b3fc2
Magoji91 <smsanda2004@gmail.com>
2016-10-28 17:31:30 -0200

    Update Run_analysis.R

--- Run_analysis.R
+++ Run_analysis.R
@@ -5,7 +5,7 @@ http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartpho
 
 Here are the data for the project:
 
-https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
 You should create one R script called run_analysis.R that does the following:
 
