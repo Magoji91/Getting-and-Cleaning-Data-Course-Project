1d94d426fbe81b33f60ece1c1724b3046ef8ef37
Magoji91 <smsanda2004@gmail.com>
2016-10-31 10:04:32 -0200

    Update Run_analysis.R

--- Run_analysis.R
+++ Run_analysis.R
@@ -10,72 +10,69 @@ http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartpho
 You should create one R script called run_analysis.R that does the following:
 
   
-I-)Merges the training and the test sets to create one data set.
-1.	How to read the arquive: features.txt    
-    features<-read.table("./data/UCI HAR Dataset/features.txt",stringsAsFactors=F)
-    
-    
-2.	How to read the arquive: activity_labels.txt
-    activity_label<-read.table("./data/UCI HAR Dataset/activity_labels.txt",stringsAsFactors=F)
-    
-    
-3.	How to read the subjects from: the testing and training sets
+I-) Merges the training and the test sets to create one data set.
+    PROCEDURE:
+1.	Read the arquive: features.txt    
+    features<-read.table("./data/UCI HAR Dataset/features.txt",stringsAsFactors=F)   
+  
+2.	Read the arquive: activity_labels.txt
+    activity_label<-read.table("./data/UCI HAR Dataset/activity_labels.txt",stringsAsFactors=F)   
+  
+3.	Read the subjects from: the testing and training sets
     testing_subjects<-read.table("./data/UCI HAR Dataset/test/subject_test.txt")
     training_subjects<-read.table("./data/UCI HAR Dataset/train/subject_train.txt")
-    
-4.	How to add the subjects from the testing set to the new dataset
-    tidy_dataset<-testing_subjects
-   
-5.	How to add the subjects from the training set to the new dataset
-    tidy_dataset<-rbind(tidy_dataset,training_subjects)
-    
-6.	How to replace the column name to Subjects
+  
+4.	Add the subjects from the testing set to the new dataset
+    tidy_dataset<-testing_subjects  
+  
+5.	Add the subjects from the training set to the new dataset
+    tidy_dataset<-rbind(tidy_dataset,training_subjects)  
+  
+6.	Replace the column name to Subjects
     colnames(tidy_dataset)[1]<-"Subjects"  
-
-7.	How to read the activities of the subjects from both the testing and training sets
-    
+  
+7.	Read the activities of the subjects from both the testing and training sets    
     test_activity<-read.table("./data/UCI HAR Dataset/test/Y_test.txt")
     train_activity<-read.table("./data/UCI HAR Dataset/train/Y_train.txt")
     
-    subject_activity<-test_activity ### Adding the testing activity 
-    subject_activity<-rbind(subject_activity,train_activity) ### Appending the training activity
-    
-8.	How to replace the activities by their name from the activity_label dataframe
+    subject_activity<-test_activity 
+    subject_activity<-rbind(subject_activity,train_activity) 
+  
+8.	Replace the activities by their name from the activity_label dataframe
       for(i in 1:nrow(tidy_dataset)){
       index<-which(activity_label[,1] == subject_activity[i,1])
       tidy_dataset$Activity[i]<-activity_label[index,2]
-    }
-    
-9.	How to read the 561 features readings for each subjects from both the testing and training sets
+    }    
+9.	Read the 561 features readings for each subjects from both the testing and training sets
     test_feature_readings<-read.table("./data/UCI HAR Dataset/test/X_test.txt")
     train_feature_readings<-read.table("./data/UCI HAR Dataset/train/X_train.txt")
-   
-10.	Adding the features readings of testing
-feature_readings<-test_feature_readings
-
-11.	Appending the features readings of training
-rbind(feature_readings,train_feature_readings)   
-    
-12.	Replacing the names of 561 columns with feature readings in the “tidy_dataset”  with their features names from the features dataframe  
+  
+10.	Add the features readings of testing
+    feature_readings<-test_feature_readings
+  
+11.	Append the features readings of training
+    rbind(feature_readings,train_feature_readings) 
+  
+12.	Replace the names of 561 columns with feature readings in the “tidy_dataset”  with their features names from the features dataframe  
     for(i in 1:ncol(feature_readings))
     {
       tidy_dataset<-cbind(tidy_dataset,feature_readings[,i])
       names(tidy_dataset)[ncol(tidy_dataset)]<-features[i,2]
-    }
-    
+    }     
   
 II-) Extracts only the measurements on the mean and standard deviation for each measurement.
-1.	Measuring the mean and standard deviation of the columns 
-    extract<-grep("(.*)(mean|std)[Freq]?(.*)[/(/)]$|(.*)(mean|std)(.*)()-[X|Y|Z]$",colnames(tidy_dataset),value=T)    
-
+    PROCEDURE:
+1.	Measure the mean and standard deviation of the columns 
+    extract<-grep("(.*)(mean|std)[Freq]?(.*)[/(/)]$|(.*)(mean|std)(.*)()-[X|Y|Z]$",colnames(tidy_dataset),value=T)     
   
 III-) Uses descriptive activity names to name the activities in the data set.
- 1.	Sorting the tidy_dataset with the extracted columns
-    tidy_dataset<-tidy_dataset[,c("Subjects","Activity",extract)]
-
+    PROCEDURE:
+ 1.	Sort the tidy_dataset with the extracted columns
+    tidy_dataset<-tidy_dataset[,c("Subjects","Activity",extract)] 
   
 IV-) Appropriately labels the data set with descriptive variable names.
- 1.	REPLACING THE ACTIVITY LABELS WITH DESCRIPTIVE NAMES:
+    PROCEDURE:
+1.	REPLACE THE ACTIVITY LABELS WITH DESCRIPTIVE NAMES:
     
     tidy_dataset$Activity<-gsub("WALKING_UPSTAIRS","Walking Up",tidy_dataset$Activity)
     tidy_dataset$Activity<-gsub("WALKING_DOWNSTAIRS","Walking Down",tidy_dataset$Activity)
@@ -84,14 +81,15 @@ IV-) Appropriately labels the data set with descriptive variable names.
     tidy_dataset$Activity<-gsub("STANDING","Standing",tidy_dataset$Activity)
     tidy_dataset$Activity<-gsub("LAYING","Laying",tidy_dataset$Activity)
   
- 2.	HOW TO REPLACE THE COLUMN NAMES WITH DESCRIPTIVE NAMES: 
-A.	Striping the parenthesis from the column names
+2.	REPLACE THE COLUMN NAMES WITH DESCRIPTIVE NAMES: 
+A.	Strip the parenthesis from the column names
     colnames(tidy_dataset)<-gsub("[/(/)]","",colnames(tidy_dataset))
     
-B.	Striping the hyphens from the column names and replacing it with underscores
+B.	Strip the hyphens from the column names and replacing it with underscores
     colnames(tidy_dataset)<-gsub("-","_",colnames(tidy_dataset))
 
 V-) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+    PROCEDURE:
 1.	Check if the data.table package is installed 
 2.	Download if not 
 3.	Load the package
@@ -107,7 +105,6 @@ V-) From the data set in step 4, creates a second, independent tidy data set wit
     }
     
 4.	Convert the data frame to data table
-    tidy_dataset = data.table(tidy_dataset)
-    
+    tidy_dataset = data.table(tidy_dataset)   
 5. Separate data by subject and activity of each of the subjects to allow the implementation of future calculations
     write.csv(tidy_dataset,"Tidy_Data.csv",row.names=F)
